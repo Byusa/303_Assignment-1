bc2c7f601bfe16b538d2b516bda97ddaa3dd20af
Serge Jabo Byusa <jabo.byusa@mail.mcgill.ca>
2018-02-03 18:42:51 -0500

    cust

--- Bank/src/bank/Customer.java
+++ Bank/src/bank/Customer.java
@@ -15,30 +15,44 @@ public class Customer extends Bank{
 	private String CustomersName;
 	private long CustomersID;
 	private double discount;
-        private double amount;
-        
+        // A customer has a name, a unique customer number, and a discount percentage.
    private long IDGenerator (){
 	   long leftLimit = 1L;
     	   long rightLimit = 10L;
            long generatedLong = leftLimit + (long) (Math.random() * (rightLimit - leftLimit));
 	   return generatedLong;
    }
-  public long getID(){
+   private long getID(){
 	  long Id = IDGenerator();
+           HashSet<Long> store = new HashSet<Long>();
+           while(!store.contains(Id)){
+               Id = IDGenerator();
+           }
+           store.add(Id);
 	  return Id;
   }
-    
-    public Customer(String NewName, long ID){
-          ID = IDGenerator();
-          CustomersID = ID;
-	  CustomersName = NewName;
+   public String toString() {
+        return this.CustomersName + "," + this.CustomersID + "," + this.discount;
     }
-	private boolean isAmountValid(String input){
-		
-		if(amount>= 0){
-			return true;
-		} 
-			return false;
-	}
+     public float CustomerDiscount(){return 0.15f;}  //interest rate
+        public void CustomerDiscounts(){        
+            System.out.print(0.15);
+        }  //interest rate
+
+   /*
+   public void Idi (){
+       System.out.print(getID);
+   }
+    public void name (){
+        
+       System.out.print(name);
+   }*/
+    public Customer(String NewName, long ID, float Newdiscount ){
+          CustomersName = NewName;
+          ID = getID();
+          CustomersID = ID;
+          Newdiscount = CustomerDiscount();
+          discount =Newdiscount;
+    }
 	
 }
