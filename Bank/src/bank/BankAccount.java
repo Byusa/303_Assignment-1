83c5c7251d20447ba416a6eb4f955f488bdafe75
Serge Jabo Byusa <jabo.byusa@mail.mcgill.ca>
2018-02-03 18:43:28 -0500

    bk

--- Bank/src/bank/BankAccount.java
+++ Bank/src/bank/BankAccount.java
@@ -1,35 +1,31 @@
-package bank;
+/*package bank;
 
 interface Transactions{
 	float rateOfInterest(); //added
-	public void deposit (double newAmount);
-	public Double withdraw (double newAmount);
+        float chargeRate();
+	void deposit (double newAmount);
+	double withdraw (double newAmount);
 }
-public class BankAccount implements Transactions{ //CheckingsAccount is the child of Customer(a customer owns a checkingsAccount)
-	private double amount;
-	CheckingsAccount chq = new CheckingsAccount(amount);
-	System.out.println("Customer's checking's account discount"+chq.discount);  
-	
-	public double getAmount(){
-	     return amount;
-	}
-	
-	 public BankAccount(){
-      		balance = 0;
-         }
-	public float rateOfInterest(){return 9.15f;}  //added
-
-   	public BankAccount(double initialAmount){//constructor with input amount
+class BankAccount implements Transactions{ //CheckingsAccount is the child of Customer(a customer owns a checkingsAccount)
+       /* private double amount;
+        public BankAccount(){ //construct 1
+            double amount=0.0;
+            this.amount= amount;
+        }
+   	public BankAccount(double initialAmount){//constructor 2 with input amount
       		amount = initialAmount;
 	}
-	public void deposit (double newAmount){//setter method=deposit
-		if(isAmountValid(newAmount)){
-			double newBalance = balance + amount; 
-			balance = newBalance;
-		}
+        public float rateOfInterest(){return 0.09f;}  //interest rate
+        public float chargeRate(){return 0.12f;}      //charge rate
+        
+        private boolean isAmountValid(double amount){
+		if(amount>= 0){
+			return true;
+		} 
+			return false;
 	}
-	
-	public double withdraw(double newAmount){
+        
+        public double withdraw(double newAmount){ //getter
 		double Taxedamount =tax(newAmount);
 		if((isAmountValid(newAmount)) &&(newAmount<amount)){
 			amount=amount-newAmount-Taxedamount;
@@ -38,9 +34,51 @@ public class BankAccount implements Transactions{ //CheckingsAccount is the chil
 		}else{
 			System.out.println("Insufficient balance");
 		}
-		balance=amount;
+		double balance=amount;
 	    return balance;
 	}
+        
+        public double getAmount(){
+	     return amount;
+	}
+        
+         public void deposit (double newAmount){//setter method=deposit
+		if(isAmountValid(newAmount)){
+			double newBalance = balance + amount; 
+			balance = newBalance;
+		}
+	}
+        
+   
+        
+        
+       
+        
+        private boolean isAmountValid(String input){
+		
+	if(amount>= 0){
+		return true;
+	} 
+		return false;
+    }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+	
+	
+	 
+	
+	
+	
 	//public double get
 	private boolean isAmountValid(double amount){
 	//try{ amount}catch(ArithmeticException e){System.out.println(e);}  	
@@ -59,7 +97,7 @@ public class BankAccount implements Transactions{ //CheckingsAccount is the chil
 	
 	private double transferFromCheqsToSavings(double amount){
 	    	return amount;
-	}
+	}*/
 }
 //balance public //
 //tax called from withdraw
