a6a8e3a6019e9ed7ac7561f00bcbefe365b113bb
Serge Jabo Byusa <jabo.byusa@mail.mcgill.ca>
2018-02-04 23:28:22 -0500

    bkac

--- Bank/src/bank/BankAccount.java
+++ Bank/src/bank/BankAccount.java
@@ -1,3 +1,13 @@
+//package pinmain;
+/*File Name: BankAccount.java
+Developers: <<Serge Jabo Byusa>>
+Purpose: << This is where all the banks are kept and where all the transaction are made>>
+Inputs: <<None>> 
+Outputs: <<None>> 
+Modifications
+==========
+<<S.B.J>> <<29th Jan>> <<created and made a inputValidation() method better>>*/
+
 package bank;
 
 interface Transactions{
@@ -6,22 +16,39 @@ interface Transactions{
 	void deposit (double newAmount);
 	double withdraw (double newAmount);
 }
-class BankAccount { //CheckingsAccount is the child of Customer(a customer owns a checkingsAccount)//implements Transactions
+public abstract class BankAccount { //CheckingsAccount is the child of Customer(a customer owns a checkingsAccount)//implements Transactions
   private double balance;
   private int accountNumber;
   //static cause it is shared by all customers
   private static int lastAccountNumber = 260600000;
 
-    public BankAccount(double intialBalance){
-        balance = intialBalance;
+    public BankAccount(double intialBalance,int accountNumber){
+        if(intialBalance>0){
+            balance = intialBalance;
+            accountNumber = lastAccountNumber + 1;
+            lastAccountNumber = accountNumber;
+        }else{
+            balance=0;
         accountNumber = lastAccountNumber + 1;
-        lastAccountNumber = accountNumber;
+        lastAccountNumber = accountNumber;}
     }
-    public void deposit(double depositAmount){
+    public BankAccount(){
+        if(balance>0){
+            this.balance = balance;
+            this.accountNumber = lastAccountNumber + 1;
+            this.lastAccountNumber = accountNumber;
+        }else{
+            this.balance=0;
+        this.accountNumber = lastAccountNumber + 1;
+        this.lastAccountNumber = accountNumber;}
+    }
+    public boolean deposit(double depositAmount){
         if(depositAmount<0){
             System.out.println("Invalid Input, there is no negative money talk to our loan system!!!");
+            return false;
         }else{
           balance += depositAmount;
+          return true;
         }
     }
     public boolean withdraw(double withdrawAmount){
@@ -36,19 +63,33 @@ class BankAccount { //CheckingsAccount is the child of Customer(a customer owns 
             return true;
         }
     }
-    /*private boolean isAmountValid(double amount){
+    private boolean isAmountValid(double amount){
 		if(amount>= 0){
 			return true;
 		} 
 			return false;
-	}*/
-    public int getNumber(){
+	}
+    public int getaccountNumber(){
         return accountNumber;
     }
     public double getBalance(){
         return balance;
     }
-
+    
+    public boolean transfer(double amountToTransfer, BankAccount secondAccount) {
+	boolean withdrawalSuccessful = this.withdraw(amountToTransfer);	//first withdraw the money from the bank account
+	if(!(withdrawalSuccessful)) {		//if withdrawal is unsuccessful return false
+		return false;
+	}
+	boolean transferSuccessful = secondAccount.deposit(amountToTransfer);
+		
+	if(transferSuccessful)		//if the deposit to second account was successful return true
+		return true;
+	else
+		return false;			//otherwise false;
+}
+	
+  public abstract String getType();
 }
 /*  
         
