ad8ac8a22dd39138ab5d3da628b73409e8e9c74e
Serge Jabo Byusa <jabo.byusa@mail.mcgill.ca>
2018-02-06 02:14:39 -0500

    Done

--- Bank/src/bank/BankAccount.java
+++ Bank/src/bank/BankAccount.java
@@ -1,27 +1,24 @@
-//package pinmain;
 /*File Name: BankAccount.java
 Developers: <<Serge Jabo Byusa>>
-Purpose: << This is where all the banks are kept and where all the transaction are made>>
+Purpose: << This is act as parent of checkings and savings account and does all there transactions>>
 Inputs: <<None>> 
-Outputs: <<None>> 
+Outputs: <<>> 
 Modifications
 ==========
-<<S.B.J>> <<29th Jan>> <<created and made a inputValidation() method better>>*/
-
+<<S.B.J>> <<2nd feb>> <<created and made a made it better() method better>>*/
 package bank;
 
-interface Transactions{
-	float rateOfInterest(); //added
-        float chargeRate();
-	void deposit (double newAmount);
-	double withdraw (double newAmount);
-}
-public abstract class BankAccount { //CheckingsAccount is the child of Customer(a customer owns a checkingsAccount)//implements Transactions
+public  class BankAccount { //CheckingsAccount is the child of Customer(a customer owns a checkingsAccount)//implements Transactions
   private double balance;
   private int accountNumber;
   //static cause it is shared by all customers
   private static int lastAccountNumber = 260600000;
-
+// Developers: <<Serge Jabo Byusa>>
+// Purpose: <<Constructor with input>>
+// Inputs: <<None>> 
+// Outputs: <<Balance and accnout number>> 
+// Side-effects: <<None>>
+// Special Notes: <<None>>
     public BankAccount(double intialBalance,int accountNumber){
         if(intialBalance>0){
             balance = intialBalance;
@@ -32,16 +29,23 @@ public abstract class BankAccount { //CheckingsAccount is the child of Customer(
         accountNumber = lastAccountNumber + 1;
         lastAccountNumber = accountNumber;}
     }
+// Developers: <<Serge Jabo Byusa>>
+// Purpose: <<Constructor without  input helps for declaration>>
+// Inputs: <<None>> 
+// Outputs: <<NOne>> 
+// Side-effects: <<None>>
+// Special Notes: <<None>>
     public BankAccount(){
-        if(balance>0){
-            this.balance = balance;
-            this.accountNumber = lastAccountNumber + 1;
-            this.lastAccountNumber = accountNumber;
-        }else{
-            this.balance=0;
+        this.balance=0;
         this.accountNumber = lastAccountNumber + 1;
-        this.lastAccountNumber = accountNumber;}
+        this.lastAccountNumber = accountNumber;
     }
+// Developers: <<Serge Jabo Byusa>>
+// Purpose: <<To deposit money in a give bank account>>
+// Inputs: <<depositAmount>> 
+// Outputs: <<true or false if you can deposit or if yes it changes the balance>> 
+// Side-effects: <<None>>
+// Special Notes: <<None>>
     public boolean deposit(double depositAmount){
         if(depositAmount<0){
             System.out.println("Invalid Input, there is no negative money talk to our loan system!!!");
@@ -51,6 +55,12 @@ public abstract class BankAccount { //CheckingsAccount is the child of Customer(
           return true;
         }
     }
+// Developers: <<Serge Jabo Byusa>>
+// Purpose: <<To deposit money in a give bank account>>
+// Inputs: <<WithdrawAmount>> 
+// Outputs: <<true or false if you can withdraw or if yes it changes the balance>> 
+// Side-effects: <<None>>
+// Special Notes: <<None>>
     public boolean withdraw(double withdrawAmount){
         if (withdrawAmount > balance){    
             System.out.println("Sorry, Insufficient Funds!!!");
@@ -63,19 +73,42 @@ public abstract class BankAccount { //CheckingsAccount is the child of Customer(
             return true;
         }
     }
+// Developers: <<Serge Jabo Byusa>>
+// Purpose: <<To deposit money in a give bank account>>
+// Inputs: <<amount>> 
+// Outputs: <<Checks if the amount is valid or not>> 
+// Side-effects: <<None>>
+// Special Notes: <<None>>
     private boolean isAmountValid(double amount){
 		if(amount>= 0){
 			return true;
 		} 
 			return false;
 	}
+// Developers: <<Serge Jabo Byusa>>
+// Purpose: <<to get the accountNumber>>
+// Inputs: <<None>> 
+// Outputs: <<returns the accountNumber>> 
+// Side-effects: <<None>>
+// Special Notes: <<None>>
     public int getaccountNumber(){
         return accountNumber;
     }
+// Developers: <<Serge Jabo Byusa>>
+// Purpose: <<to get the balance>>
+// Inputs: <<None>> 
+// Outputs: <<returns the balance>> 
+// Side-effects: <<None>>
+// Special Notes: <<None>>
     public double getBalance(){
         return balance;
     }
-    
+ // Developers: <<Serge Jabo Byusa>>
+// Purpose: <<to get transfer Money from one account to the otherone>>
+// Inputs: <<the amount To Transfer, the secondAccount>> 
+// Outputs: <<returns true if you can trasfer and changes the balance>> 
+// Side-effects: <<None>>
+// Special Notes: <<None>>
     public boolean transfer(double amountToTransfer, BankAccount secondAccount) {
 	boolean withdrawalSuccessful = this.withdraw(amountToTransfer);	//first withdraw the money from the bank account
 	if(!(withdrawalSuccessful)) {		//if withdrawal is unsuccessful return false
@@ -89,76 +122,5 @@ public abstract class BankAccount { //CheckingsAccount is the child of Customer(
 		return false;			//otherwise false;
 }
 	
-  public abstract String getType();
+  //public abstract String getType();
 }
-/*  
-        
-        
-        public double withdraw(double newAmount){ //getter
-		double Taxedamount =tax(newAmount);
-		if((isAmountValid(newAmount)) &&(newAmount<amount)){
-			amount=amount-newAmount-Taxedamount;
-			//double newBalance = balance - amount;
-      			//balance = newBalance;
-		}else{
-			System.out.println("Insufficient balance");
-		}
-		double balance=amount;
-	    return balance;
-	}
-        
-         public void deposit (double newAmount){//setter method=deposit
-		if(isAmountValid(newAmount)){
-			double newBalance = balance + amount; 
-			balance = newBalance;
-		}
-	}
-        
-   
-        
-        
-       
-        
-        private boolean isAmountValid(String input){
-		
-	if(amount>= 0){
-		return true;
-	} 
-		return false;
-    }
-        
-        
-        */
-        
-        
-        
-        
-        
-        
-        
-        
-    
-	
-	
-	 
-	
-	
-	/*
-	//public double get
-	private boolean isAmountValid(double amount){
-	//try{ amount}catch(ArithmeticException e){System.out.println(e);}  	
-		if(amount>= 0){
-			return true;
-		}
-	
-			return false;
-	}
-	private double transferFromCheqsToSavings(double amount){
-	    	return amount;
-	}
-}*/
-//balance public //
-//tax called from withdraw
-//will change later deposit
-
-  
