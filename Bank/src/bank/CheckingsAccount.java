e6b46e22dbc6b0d32a066f16999bba985ed54290
Serge Jabo Byusa <jabo.byusa@mail.mcgill.ca>
2018-02-06 02:15:20 -0500

    Done

--- Bank/src/bank/CheckingsAccount.java
+++ Bank/src/bank/CheckingsAccount.java
@@ -1,37 +1,126 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*File Name: BankAccount.java
+Developers: <<Serge Jabo Byusa>>
+Purpose: << This is act as parent of checkings and savings account and does all there transactions>>
+Inputs: <<None>> 
+Outputs: <<>> 
+Modifications
+==========
+<<S.B.J>> <<2nd feb>> <<created and made a made it better() method better>>*/
 package bank;
 
-/**
- *
- * @author jabo.byusa
- */
-
-public class CheckingAccount extends BankAccount {
-	
-	public CheckingAccount(double balance,int accountNum) {
-		super(balance,accountNum);
+public  class BankAccount { //CheckingsAccount is the child of Customer(a customer owns a checkingsAccount)//implements Transactions
+  private double balance;
+  private int accountNumber;
+  //static cause it is shared by all customers
+  private static int lastAccountNumber = 260600000;
+// Developers: <<Serge Jabo Byusa>>
+// Purpose: <<Constructor with input>>
+// Inputs: <<None>> 
+// Outputs: <<Balance and accnout number>> 
+// Side-effects: <<None>>
+// Special Notes: <<None>>
+    public BankAccount(double intialBalance,int accountNumber){
+        if(intialBalance>0){
+            balance = intialBalance;
+            accountNumber = lastAccountNumber + 1;
+            lastAccountNumber = accountNumber;
+        }else{
+            balance=0;
+        accountNumber = lastAccountNumber + 1;
+        lastAccountNumber = accountNumber;}
+    }
+// Developers: <<Serge Jabo Byusa>>
+// Purpose: <<Constructor without  input helps for declaration>>
+// Inputs: <<None>> 
+// Outputs: <<NOne>> 
+// Side-effects: <<None>>
+// Special Notes: <<None>>
+    public BankAccount(){
+        this.balance=0;
+        this.accountNumber = lastAccountNumber + 1;
+        this.lastAccountNumber = accountNumber;
+    }
+// Developers: <<Serge Jabo Byusa>>
+// Purpose: <<To deposit money in a give bank account>>
+// Inputs: <<depositAmount>> 
+// Outputs: <<true or false if you can deposit or if yes it changes the balance>> 
+// Side-effects: <<None>>
+// Special Notes: <<None>>
+    public boolean deposit(double depositAmount){
+        if(depositAmount<0){
+            System.out.println("Invalid Input, there is no negative money talk to our loan system!!!");
+            return false;
+        }else{
+          balance += depositAmount;
+          return true;
+        }
+    }
+// Developers: <<Serge Jabo Byusa>>
+// Purpose: <<To deposit money in a give bank account>>
+// Inputs: <<WithdrawAmount>> 
+// Outputs: <<true or false if you can withdraw or if yes it changes the balance>> 
+// Side-effects: <<None>>
+// Special Notes: <<None>>
+    public boolean withdraw(double withdrawAmount){
+        if (withdrawAmount > balance){    
+            System.out.println("Sorry, Insufficient Funds!!!");
+            return false;
+        }else if(withdrawAmount<0){
+            System.out.println("Invalid Input, there is no negative money talk to our loan system!!!");
+            return false;
+        } else {
+            balance -= withdrawAmount;
+            return true;
+        }
+    }
+// Developers: <<Serge Jabo Byusa>>
+// Purpose: <<To deposit money in a give bank account>>
+// Inputs: <<amount>> 
+// Outputs: <<Checks if the amount is valid or not>> 
+// Side-effects: <<None>>
+// Special Notes: <<None>>
+    private boolean isAmountValid(double amount){
+		if(amount>= 0){
+			return true;
+		} 
+			return false;
 	}
-	
-	public boolean deposit(double amountToDeposit) {
-		return super.deposit(amountToDeposit);
+// Developers: <<Serge Jabo Byusa>>
+// Purpose: <<to get the accountNumber>>
+// Inputs: <<None>> 
+// Outputs: <<returns the accountNumber>> 
+// Side-effects: <<None>>
+// Special Notes: <<None>>
+    public int getaccountNumber(){
+        return accountNumber;
+    }
+// Developers: <<Serge Jabo Byusa>>
+// Purpose: <<to get the balance>>
+// Inputs: <<None>> 
+// Outputs: <<returns the balance>> 
+// Side-effects: <<None>>
+// Special Notes: <<None>>
+    public double getBalance(){
+        return balance;
+    }
+ // Developers: <<Serge Jabo Byusa>>
+// Purpose: <<to get transfer Money from one account to the otherone>>
+// Inputs: <<the amount To Transfer, the secondAccount>> 
+// Outputs: <<returns true if you can trasfer and changes the balance>> 
+// Side-effects: <<None>>
+// Special Notes: <<None>>
+    public boolean transfer(double amountToTransfer, BankAccount secondAccount) {
+	boolean withdrawalSuccessful = this.withdraw(amountToTransfer);	//first withdraw the money from the bank account
+	if(!(withdrawalSuccessful)) {		//if withdrawal is unsuccessful return false
+		return false;
 	}
+	boolean transferSuccessful = secondAccount.deposit(amountToTransfer);
+		
+	if(transferSuccessful)		//if the deposit to second account was successful return true
+		return true;
+	else
+		return false;			//otherwise false;
+}
 	
-	public boolean withdraw(double amountToWithdraw, double discountPercent) {
-		double withdrawalCharge = 1 - discountPercent;		//1 dollar is the default charge, subtract from 1 the discount user receives		
-		amountToWithdraw += withdrawalCharge; 				//add withdrawal charge to the total amount to be withdrawn
-		return super.withdraw(amountToWithdraw);					//call the withdraw method from superclass with new withdrawal value				
-	}
-	
-	public boolean transfer(double amountToTransfer, BankAccount secondAccount) {
-		return super.transfer(amountToTransfer, secondAccount);
-	}
-	
-	public String getType() {
-		return "checkings";
-	}
-	
+  //public abstract String getType();
 }
